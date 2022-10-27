5c0d5cb301db43f60d37f242cdbe91499827fa5d
Robson Fernando Martins <rbsndev3@gmail.com>
2022-10-27 19:21:27 -0300

    feat: header base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import Header from "./components/header";
+import { MotionContainerTop } from "./components/MotionContainerTop";
 import "./styles/globals.sass";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <MotionContainerTop>
+        <div
+          style={{
+            background: "lightblue",
+            width: "100%",
+            height: "calc(100vh - 6rem)",
+            border: "2px solid black",
+          }}
+        >
+          <div>
+            <h2>Olá,</h2> <h3>Eu sou o Robson F. Martins</h3>
+            <button>ver projetos</button>
+          </div>
+
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </MotionContainerTop>
     </div>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./header.module.sass";
 import { HiOutlineMenu } from "react-icons/hi";
-import { MotionContainer } from "../MotionContainer";
+import { MdOutlineDarkMode, MdDarkMode } from "react-icons/md";
 
 const Header = () => {
+  const [togle, setTogle] = useState(false);
+
   return (
-    <MotionContainer>
-      <header className={styles.header}>
-        <div className={styles.header_logo}>
-          <h3>Robson F. Martins</h3>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.nav_box}>
+          <div className={styles.logo}>
+            <h3>Robson F. Martins</h3>
+          </div>
+          <div className={styles.menu_div}>
+            <HiOutlineMenu className={styles.menu_icon} size={20} />
+          </div>
         </div>
-        <nav className={styles.header_nav}>
-          <ul className={styles.header_ul}>
-            <li className={styles.header_li}>
-              <HiOutlineMenu className={styles.menu_icon} size={20} />
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </MotionContainer>
+
+        <ul className={styles.nav_ul}>
+          <li className={styles.nav_li}>
+            <h3>Home</h3>
+          </li>
+          <li className={styles.nav_li}>
+            <h3>Sobre min</h3>
+          </li>
+          <li className={styles.nav_li}>
+            <h3>Projetos</h3>
+          </li>
+          <li className={styles.nav_li}>
+            <h3>Contato</h3>
+          </li>
+          <li className={styles.nav_li} onClick={() => setTogle(!togle)}>
+            {togle ? <MdOutlineDarkMode size={20} /> : <MdDarkMode size={20} />}
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
